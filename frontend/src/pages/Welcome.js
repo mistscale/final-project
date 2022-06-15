181bd33bd97856688d4bbad0e2bdcc0619ee542e
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2022-06-15 11:47:18 +0200

    Added some code to the signup

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Welcome = () => {
-    return (
-        <div>
-            <h1>Hi there</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1506485338023-6ce5f36692df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
+      <h1>Hi there</h1>
+    </div>
+  );
+};
 
-export default Welcome; 
+export default Welcome;
