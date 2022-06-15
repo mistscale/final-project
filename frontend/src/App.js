96849dd35a1326f808b03c5d478d8b89088267c8
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2022-06-15 19:58:57 +0200

    redefined the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-// import Welcome from 'pages/Welcome';
-// import Login from 'pages/Login';
+
+import Welcome from 'pages/Welcome';
+import Login from 'pages/Login';
 import Signup from 'pages/SignUp';
-// import NewEvent from 'pages/NewEvent';
-// import Confirmation from 'pages/Confirmation';
-// import MyEvents from 'pages/MyEvents';
-// import NotFound from 'pages/NotFound';
+import NewEvent from 'pages/NewEvent';
+import Confirmation from 'pages/Confirmation';
+import MyEvents from 'pages/MyEvents';
+import NotFound from 'pages/NotFound';
+
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import user from 'reducers/user';
+
+
+
+const reducer = combineReducers({
+  user: user.reducer,
+});
+
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <ChakraProvider>
+    
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Welcome />}></Route> */}
-          {/* <Route path="/login" element={<Login />}></Route> */}
+          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
-          {/* <Route path="/newevent" element={<NewEvent />}></Route> */}
-          {/* <Route path="/confirmation" element={<Confirmation />}></Route> */}
-          {/* <Route path="/myevents" element={<MyEvents />}></Route> */}
-          {/* <Route path="*" element={<NotFound />}></Route> */}
+          <Route path="/newevent" element={<NewEvent />}></Route> 
+          <Route path="/confirmation" element={<Confirmation />}></Route>
+          <Route path="/myevents" element={<MyEvents />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
-    </ChakraProvider>
+    </Provider>
   );
 };
