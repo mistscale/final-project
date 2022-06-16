845dcb785f19313ef9279658e1930f0ccb3fc536
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2022-06-16 10:32:49 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ import user from 'reducers/user';
 import Welcome from 'pages/Welcome';
 import Login from 'pages/Login';
 import Signup from 'pages/SignUp';
-import NewEvent from 'pages/NewEvent';
-import Confirmation from 'pages/Confirmation';
+// import NewEvent from 'pages/NewEvent';
+// import Confirmation from 'pages/Confirmation';
 import MyEvents from 'pages/MyEvents';
 import NotFound from 'pages/NotFound';
 
@@ -27,8 +27,6 @@ export const App = () => {
           <Route path="/" element={<Welcome />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/newevent" element={<NewEvent />}></Route>
-          <Route path="/confirmation" element={<Confirmation />}></Route>
           <Route path="/myevents" element={<MyEvents />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
