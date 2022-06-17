d0321e30f2d8ad9d65f3dbaa3f66b43dc17a118a
Lisen Lundgren <lisolundgren@gmail.com>
2022-06-17 16:07:50 +0200

    add illustration to welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import illustration from '../images/illustration.jpg';
+
 const Welcome = () => {
 	const navigate = useNavigate();
 	const handleOnClick = () => {
@@ -9,7 +11,13 @@ const Welcome = () => {
 	return (
 		<div className='container'>
 			<h1>Welcome to event planner app</h1>
-            <p>To create events you need to log in to your account or sign up</p>
+			<img
+				src={illustration}
+				className='illustration'
+				alt='illustration of a party'
+                
+			/>
+			<p>To create events you need to log in to your account or sign up</p>
 			<button onClick={handleOnClick}>Click here to login/signup</button>
 		</div>
 	);
