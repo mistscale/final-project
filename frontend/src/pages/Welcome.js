94f945fa4d3cc152e30ae16e7553068c42637583
Lisen Lundgren <lisolundgren@gmail.com>
2022-06-17 15:53:36 +0200

    create basic welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Welcome = () => {
-    return (
-        <div>
-            <h1>Hi there</h1>
-        </div>
-    )
-}
+	const navigate = useNavigate();
+	const handleOnClick = () => {
+		navigate('/signup');
+	};
+	return (
+		<div className='container'>
+			<h1>Welcome to event planner app</h1>
+            <p>To create events you need to log in to your account or sign up</p>
+			<button onClick={handleOnClick}>Click here to login/signup</button>
+		</div>
+	);
+};
 
-export default Welcome; 
+export default Welcome;
