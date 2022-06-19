1a7c756070ca3f30f3ca009fa14de7f6504b425c
Lisen Lundgren <lisolundgren@gmail.com>
2022-06-19 13:26:44 +0200

    fetch event with accessToken

--- frontend/src/pages/MyEvents.js
+++ frontend/src/pages/MyEvents.js
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:8080/events';
+import user from 'reducers/user';
 
 const MyEvents = () => {
-	const [myEvents, setMyEvents] = useState();
+	const accessToken = useSelector((store) => store.user.accessToken);
+	const userId = useSelector((store) => store.user.userId);
+
+	const navigate = useNavigate();
 
 	useEffect(() => {
-		fetchEvents();
-	}, []);
+		if (!accessToken) {
+			navigate('/');
+		}
+	}, [accessToken, navigate]);
 
-	const options = {
-		method: 'GET',
-		headers: {
-			Authorization:
-				'3253b0166c620f4c5d9bb5b8674163736bd08a2df220000fa4c2d269a5a2cf2f189c9b60e30b67838693500328370457d6bb9868f2240509f063e275799ce5b3e458512fe0334b6ff436462107ccb64685ee4914ceb3dbb2960e299a86c0a0ec5e6e9dec8130cdaabdf94539d40423a385135be7b824ee99b383f77791bd045c',
-		},
-	};
+	useEffect(() => {
+		const options = {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: accessToken,
+			},
+			// body: JSON.stringify({
+			// 	title: title,
+			// 	date: date,
+			// 	location: location,
+			// 	category: category,
+			// 	details: details,
+			// 	user: userId,
+			// }),
+		};
 
-	const fetchEvents = () => {
-		fetch(API_URL, options)
+		fetch(
+			`https://final-project-eventapp.herokuapp.com/events/${userId}`,
+			options
+		)
 			.then((res) => res.json())
 			.then((data) => {
-				setMyEvents(data);
 				console.log(data);
 			});
-	};
+	});
 };
 
 export default MyEvents;
