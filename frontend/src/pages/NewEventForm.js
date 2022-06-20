f36f56621f1b5011bfee7236269cdf30270c3403
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-06-20 18:51:08 +0200

    chaged navigate

--- frontend/src/pages/NewEventForm.js
+++ frontend/src/pages/NewEventForm.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector, batch } from 'react-redux';
+import { EVENT_URL } from 'utils/urls';
 import { useNavigate } from 'react-router-dom';
-import { API_URL } from 'utils/urls';
-import "../index.css"
 import user from 'reducers/user';
+import { useSelector } from 'react-redux';
 
+
+import Alert from '@mui/material/Alert';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -16,133 +19,164 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Alert from '@mui/material/Alert';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
-const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState(null);
 
-    const [mode, setMode] = useState('login');
-    const theme = createTheme();
+const NewEventForm = () => {
+	const [title, setTitle] = useState('');
+	const [date, setDate] = useState('');
+	const [location, setLocation] = useState('');
+	const [category, setCategory] = useState('');
+	const [details, setDetails] = useState('');
+	
+	const theme = createTheme();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const accessToken = useSelector((store) => store.user.accessToken);
-  
+	const accessToken = useSelector((store) => store.user.accessToken);
 
-    useEffect(() => {
-        if (accessToken) {
-            //navigate('/myevents');
-            navigate('/neweventform');
+	const userId = useSelector((store) => store.user.userId)
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+        if (!accessToken) {
+            navigate('/login');
         }
     }, [accessToken]);
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
+	const onFormSubmit = (event) => {
+		event.preventDefault();
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username: username, password: password }),
-        };
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization:
+					accessToken,
+			},
+			body: JSON.stringify({
+				title: title,
+				date: date,
+				location: location,
+				category: category,
+				details: details,
+				user: userId
+			}),
+		};
+		fetch(EVENT_URL, options)
+			.then((res) => res.json())
+			.then((data) => {
+				// window.location.reload()
+				navigate('/myevents');
+			});
+	};
 
-        fetch(API_URL(mode), options)
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.success) {
-                    batch(() => {
-                        dispatch(user.actions.setUserId(data.userId));
-                        dispatch(user.actions.setAccessToken(data.accessToken));
-                        dispatch(user.actions.setUserName(data.username));
-                        dispatch(user.actions.setError(null));
-                        setErrorMessage(null);
-                    });
-                } else {
-                    batch(() => {
-                        dispatch(user.actions.setError(data.response));
-                        dispatch(user.actions.setUserId(null));
-                        dispatch(user.actions.setAccessToken(null));
-                        dispatch(user.actions.setUserName(null));
-                        setErrorMessage(data.response);
-                    });
-                }
-            });
-    };
-
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Login
-                        </Typography>
-                        <Box component="form" onSubmit={onFormSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="username"
-                                label="Username"
-                                name="username"
-                                autoComplete="username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item>
-                                    <Link href="/signup" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                            
-                        </Box>
-                    </Box>
-                    {errorMessage !== null && (
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 9,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Typography component="h1" variant="h5">
+						Create a Event
+					</Typography>
+					<Box component="form" noValidate onSubmit={onFormSubmit} sx={{ mt: 3 }}>
+					<FormControl sx={{ m: 0, width: 400 }}>
+						<InputLabel id="demo-multiple-name-label">Category</InputLabel>
+						<Select
+							labelId="category-label"
+							id="category"
+							required
+							fullWidth
+							label="Category"
+							name="category"
+							autoComplete="category"
+							value={category}
+							onChange={(e) => setCategory(e.target.value)}
+						>
+							<MenuItem value={10}>Kid's party</MenuItem>
+							<MenuItem value={20}>After work</MenuItem>
+							<MenuItem value={30}>Party</MenuItem>
+						</Select>
+					
+				  </FormControl>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								id="title"
+								label="Title"
+								name="title"
+								autoComplete="title"
+								value={title}
+								onChange={(e) => setTitle(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name="location"
+								label="Location"
+								type="location"
+								id="location"
+								autoComplete="location"
+								value={location}
+								onChange={(e) => setLocation(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name="date"
+								type="datetime-local"
+								id="date"
+								autoComplete="date"
+								value={date}
+								onChange={(e) => setDate(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name="details"
+								label="Details"
+								type="details"
+								id="details"
+								autoComplete="details"
+								value={details}
+								onChange={(e) => setDetails(e.target.value)}
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Submit
+					</Button>
+				</Box>
+			</Box>
+			{/* {errorMessage !== null && (
           <Alert severity="error">{errorMessage}</Alert>
-        )}
-                </Container>
-            </ThemeProvider>
-    
-        </>
-    );
-}
+        )} */}
+		</Container>
+		</ThemeProvider >
+	);
+};
 
-export default Login;
+export default NewEventForm;
