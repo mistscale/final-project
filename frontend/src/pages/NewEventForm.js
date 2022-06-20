788f2c6155ce55290d548c97c30c74f90d5d6f13
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-06-20 11:44:03 +0200

    styled neweventform

--- frontend/src/pages/NewEventForm.js
+++ frontend/src/pages/NewEventForm.js
@@ -1,109 +1,148 @@
-import React, { useState } from 'react';
-import { EVENT_URL } from 'utils/urls';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector, batch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { API_URL } from 'utils/urls';
+import "../index.css"
 import user from 'reducers/user';
-import { useSelector } from 'react-redux';
 
-const NewEventForm = () => {
-	const [title, setTitle] = useState('');
-	const [date, setDate] = useState('');
-	const [location, setLocation] = useState('');
-	const [category, setCategory] = useState('');
-	const [details, setDetails] = useState('');
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from '@mui/material/Alert';
 
-	const accessToken = useSelector((store) => store.user.accessToken);
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
 
-	const userId = useSelector((store) => store.user.userId)
+    const [mode, setMode] = useState('login');
+    const theme = createTheme();
 
-	const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const accessToken = useSelector((store) => store.user.accessToken);
+  
 
-	const onFormSubmit = (event) => {
-		event.preventDefault();
+    useEffect(() => {
+        if (accessToken) {
+            //navigate('/myevents');
+            navigate('/neweventform');
+        }
+    }, [accessToken]);
 
-		const options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization:
-					accessToken,
-			},
-			body: JSON.stringify({
-				title: title,
-				date: date,
-				location: location,
-				category: category,
-				details: details,
-				user: userId
-			}),
-		};
-		fetch(EVENT_URL, options)
-			.then((res) => res.json())
-			.then((data) => {
-				// window.location.reload()
-				navigate('/myevents');
-			});
-	};
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-	return (
-		<section>
-			<h1>Create Event</h1>
-			<form onSubmit={onFormSubmit}>
-				<label>
-					Event title
-					<input
-						name='title'
-						type='text'
-						value={title}
-						required
-						onChange={(e) => setTitle(e.target.value)}
-					/>
-				</label>
-				<label>
-					Location
-					<input
-						name='location'
-						type='text'
-						value={location}
-						required
-						onChange={(e) => setLocation(e.target.value)}
-					/>
-				</label>
-				<label>
-					Date
-					<input
-						name='date'
-						type='datetime-local'
-						value={date}
-						required
-						onChange={(e) => setDate(e.target.value)}
-					/>
-				</label>
-				<label>
-					Category
-					<select
-						value={category}
-						onChange={(e) => setCategory(e.target.value)}
-					>
-						<option value='N/A'></option>
-						<option value="Kid's birthday party">Kid's birthday party</option>
-						<option value='After work'>After work</option>
-						<option value='Party'>Party</option>
-					</select>
-				</label>
-				<label>
-					Details
-					<input
-						name='details'
-						type='text'
-						value={details}
-						required
-						onChange={(e) => setDetails(e.target.value)}
-					/>
-				</label>
-				<button type='submit'>Submit event</button>
-			</form>
-		</section>
-	);
-};
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: username, password: password }),
+        };
 
-export default NewEventForm;
+        fetch(API_URL(mode), options)
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.success) {
+                    batch(() => {
+                        dispatch(user.actions.setUserId(data.userId));
+                        dispatch(user.actions.setAccessToken(data.accessToken));
+                        dispatch(user.actions.setUserName(data.username));
+                        dispatch(user.actions.setError(null));
+                        setErrorMessage(null);
+                    });
+                } else {
+                    batch(() => {
+                        dispatch(user.actions.setError(data.response));
+                        dispatch(user.actions.setUserId(null));
+                        dispatch(user.actions.setAccessToken(null));
+                        dispatch(user.actions.setUserName(null));
+                        setErrorMessage(data.response);
+                    });
+                }
+            });
+    };
+
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Login
+                        </Typography>
+                        <Box component="form" onSubmit={onFormSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoComplete="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item>
+                                    <Link href="/signup" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                            
+                        </Box>
+                    </Box>
+                    {errorMessage !== null && (
+          <Alert severity="error">{errorMessage}</Alert>
+        )}
+                </Container>
+            </ThemeProvider>
+    
+        </>
+    );
+}
+
+export default Login;
