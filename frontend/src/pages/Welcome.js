8817a85358856432c606a3799169845501d30a73
Lisen Lundgren <lisolundgren@gmail.com>
2022-06-21 14:07:53 +0200

    style welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 import image from '../images/welcome.jpg';
 
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Welcome = () => {
 	const navigate = useNavigate();
@@ -14,25 +16,41 @@ const Welcome = () => {
 		navigate('/signup');
 	};
 	return (
-		<div className='container'>
-			<h1>Welcome to event planner app</h1>
-			<img
-				src={image}
-				className='illustration'
-				alt='illustration of a calendar'
-			/>
-			<p>
-				To create an event you need to log in to your account or sign up to
-				create a new account.
-			</p>
-			<Button onClick={handleOnClickLogIn} variant='outlined'>
-				Log In
-			</Button>
-			<Button onClick={handleOnClickSignUp} variant='outlined'>
-				Sign Up
-			</Button>
-		</div>
+		<WelcomeWrapper>
+			<img src={image} alt='illustration of a calendar' width='100%' />
+			<Typography variant='h6' gutterBottom>
+				Welcome to EventApp!
+			</Typography>
+			<Typography variant='body1' gutterBottom>
+				To create an event and start planning, you need to log in to your
+				account or sign up to create a new account.
+			</Typography>
+			<ButtonWrapper>
+				<Button onClick={handleOnClickLogIn} variant='outlined'>
+					Log In
+				</Button>
+				<Button onClick={handleOnClickSignUp} variant='outlined'>
+					Sign Up
+				</Button>
+			</ButtonWrapper>
+		</WelcomeWrapper>
 	);
 };
+
+const WelcomeWrapper = styled.div`
+	width: 80%;
+	max-width: 600px;
+	margin: auto;
+	background-color: #fff;
+	padding: 20px;
+	text-align: center;
+`;
+
+const ButtonWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	gap: 20px;
+	margin: 20px;
+`;
 
 export default Welcome;
